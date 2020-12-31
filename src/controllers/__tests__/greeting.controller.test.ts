5f09588ca18fc4c0f64f09155ecd1eeb1e05a877
Manuel Pascual <manupascual90@gmail.com>
2020-12-31 16:51:55 -0300

    feat: add new test

--- src/controllers/__tests__/greeting.controller.test.ts
+++ src/controllers/__tests__/greeting.controller.test.ts
@@ -1,7 +1,8 @@
 import { MockController } from '../../types'
-import { sayHi } from '../greeting.controller'
+import { sayHi, sayGoodbye } from '../greeting.controller'
 
 const sayHiMock = sayHi as MockController
+const sayGoodbyeMock = sayGoodbye as MockController
 const statusMock = jest.fn()
 const jsonMock = jest.fn()
 
@@ -11,13 +12,25 @@ const getMockResponse = () => ({
   })),
 })
 
-test('Should return status 200 and json response.', () => {
+test('SayHi: Should return status 200 and json response.', () => {
   const data = {
     message: 'Hi! Follow me on twitter @manupasc',
   }
   const responseMock = getMockResponse()
 
   sayHiMock(null, responseMock, () => null)
+
+  expect(statusMock).toHaveBeenCalledWith(200)
+  expect(jsonMock).toHaveBeenCalledWith(data)
+})
+
+test('SayGoodbye: Should return status 200 and json response.', () => {
+  const data = {
+    message: 'Bye! Thanks for passing by.',
+  }
+  const responseMock = getMockResponse()
+
+  sayGoodbyeMock(null, responseMock, () => null)
 
   expect(statusMock).toHaveBeenCalledWith(200)
   expect(jsonMock).toHaveBeenCalledWith(data)
