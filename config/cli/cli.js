7ac9a634d3cc7d1f974117cf821a9b06d7890876
Manuel Pascual <manupascual90@gmail.com>
2021-01-06 00:54:56 -0300

    refactor: create cli constants file

--- config/cli/cli.js
+++ config/cli/cli.js
@@ -1,7 +1,8 @@
+const fs = require('fs')
 const prompt = require('prompt')
 const schema = require('./schema')
 const createResource = require('./creator')
-const fs = require('fs')
+const { MESSAGES } = require('./constants')
 
 // Prompt configuration
 prompt.message = ''
@@ -13,15 +14,15 @@ async function exec() {
   try {
     const { resourceName } = await prompt.get(schema)
     const result = await createResource({ resourceName })
-    if (!result) throw new Error(2)
-    console.log('Resource created successfully!')
+    if (!result) throw new Error(MESSAGES.resourceExists)
+    console.log(MESSAGES.resourceCreated)
   } catch (err) {
     switch (err.message) {
-      case 2:
-        console.log('Resource already exists! Try again with a new identifier.')
+      case [MESSAGES.resourceExists]:
+        console.log(MESSAGES.resourceExists)
         break
       default:
-        console.log('Resource creation cancelled!')
+        console.log(MESSAGES.resourceCreationCancelled)
     }
   }
 }
